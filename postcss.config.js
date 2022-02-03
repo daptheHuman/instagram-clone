542208915480abee59150f831b8ded23f2d995d1
Dafa <dafaaqilla72@gmail.com>
2022-02-03 13:12:55 +0700

    test: optimize css for production

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable global-require */
 module.exports = {
-  plugins: [require('tailwindcss'), require('autoprefixer')],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
+  },
 };
